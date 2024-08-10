b8b95c31c920d8126ebb1ee30611533ae11d15c3
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-08-10 21:47:00 +0600

    feat(Saas): Navbar added and new font. chore(Saas): Changed tailwind.config.ts file mainly for animation and aceternity UI.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/global/Navbar";
 
 export default function Home() {
   return (
     <main >
-      Main Page
+      <Navbar/>
     </main>
   );
 }
